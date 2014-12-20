e983a24af463f5d389fabe96841d31f8c2666699
Loïc Delmaire <loic.delmaire@gmail.com>
2014-12-20 15:15:28 +0100

    Add some logs for worker for fails

--- app/workers/vacuum_worker.rb
+++ app/workers/vacuum_worker.rb
@@ -1,10 +1,21 @@
 class VacuumWorker
   include Sidekiq::Worker
 
-  def perform(repository_id)
-    repository = Repository.find(repository_id)
-    link_sanitizer = Vacuum::LinkSanitizer.new(repository.link)
+  def logger
+    @logger ||= Logger.new("#{Rails.root}/log/vacuum.log")
+  end
 
-    Vacuum::LootPlace.new(root_link: link_sanitizer, link: link_sanitizer, repository: repository).sack_it!
+  def perform(repository_id)
+    begin
+      raise "No id for repository" if repository_id.blank?
+
+      repository = Repository.find(repository_id)
+      raise "No repository" if repository.blank?
+
+      link_sanitizer = Vacuum::LinkSanitizer.new(repository.link)
+      Vacuum::LootPlace.new(root_link: link_sanitizer, link: link_sanitizer, repository: repository).sack_it!
+    rescue => e
+      logger.error "Unable to loot repository: #{e}"
+    end
   end
 end
